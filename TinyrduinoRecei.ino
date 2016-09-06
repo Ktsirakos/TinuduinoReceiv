651f88af54a52b87a3e7a4688c589c2a1481f8c2
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2016-09-06 22:22:06 +0300

    Initialized TInurduino Receiver

---
+++ TinyrduinoRecei.ino
@@ -0,0 +1,37 @@
+#include <RHReliableDatagram.h>
+#include <RHHardwareSPI.h>
+#include <RHGenericDriver.h>
+#include <RHDatagram.h>
+#include <RHSPIDriver.h>
+#include <RadioHead.h>
+#include <RH_RF22.h>
+#include <RHGenericSPI.h>
+
+#define SERVER_ADDRESS 2
+
+RH_RF22 driver(7,3);
+RHReliableDatagram manager(driver , SERVER_ADDRESS);
+
+uint8_t buf[RH_RF22_MAX_MESSAGE_LEN];
+
+void setup() {
+  Serial.begin(9600);
+  
+  if(!manager.init()){
+    Serial.println("init failed");
+  }
+}
+
+void loop() {
+  while(manager.available()){
+    
+  uint8_t len = sizeof(buf);
+  uint8_t from;
+  
+  if(manager.recvfromAck(buf, &len)){
+    char* textMessage[] = {(char*)buf};
+    String Text = (String)textMessage[0];
+    Serial.println(Text);
+    }
+  }
+}
